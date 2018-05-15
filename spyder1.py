d45693ed459240267c499a05d3d692b30624fbbb
xiaoheiquan <chriswang0418@163.com>
2018-05-15 16:56:23 +0800

    1st

---
+++ spyder1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  9 13:21:48 2018
+@author: Dell
+"""
+
+from urllib.request import urlopen
+import BeautifulSoup as bs
+
+# html =urlopen( "https://morvanzhou.github.io/static/scraping/basic-structure.html").read().decode('utf-8')
+# print(html)
+
